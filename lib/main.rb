df615e8e8aa6c1df3102b565de45c4a251979325
Jake Standley <jakestandley95@gmail.com>
2021-09-28 18:02:12 -0500

    Finish first draft of board design
    Designed 3 versions of the board in board_design.txt, then created 3 #display_board methods in Chess. At this point they are just displaying with a white king in every cell, so I'll need to dynamically set each set display. Also I'll probably move the display method into another class, maybe Interface.
    
    Either way, I'm done for now. It's been a long day.... Chess, you will not defeat me!

--- lib/main.rb
+++ lib/main.rb
@@ -8,9 +8,14 @@ require_relative 'chess'
 
 chess = Chess.new
 
-chess.pretty_print_board_text
+# chess.pretty_print_board_text
 
-chess.pretty_print_board_symbols
+# chess.pretty_print_board_symbols
+
+chess.display_board_v1
+chess.display_board_v2
+chess.display_board_v3
+
 
 # p board.create_valid_destination_list('Rook', 2, 2)
 # p board.create_valid_destination_list('Knight', 0, 1)
