b665a174de8d8891eb2b3ec5142d3a3e77dbd3e2
Jake Standley <jakestandley95@gmail.com>
2021-10-04 12:35:35 -0500

    Clean up player assignment in GameBuilder
    Before when GameBuilder created all the starting pieces, it set their @owner values to strings (eg 'Player 1'). Now it assigns the player1/player2 objects instead. This was needed to fix a problem with the Validator methods where it was trying to compare piece.owner to a player object and always failing even when they should have been the same. ie it was comparing player1 the object to 'Player 1'.

--- lib/game_builder.rb
+++ lib/game_builder.rb
@@ -4,6 +4,7 @@ require_relative 'player'
 require_relative 'board'
 require_relative 'interface'
 require_relative 'piece'
+require_relative 'validator'
 
 # this class is meant to build or setup the game
 class GameBuilder
@@ -13,9 +14,10 @@ class GameBuilder
     @board = Board.new
     build_starting_board
     @interface = Interface.new
+    @validator = Validator.new
   end
 
-  attr_reader :interface, :validator, :board
+  attr_reader :interface, :validator, :board, :player1, :player2
 
   private
 
@@ -25,14 +27,14 @@ class GameBuilder
   end
 
   def place_white_starting_pieces
-    place_piece(0, 0, Rook.new('White', 'Player 1'))
-    place_piece(0, 1, Knight.new('White', 'Player 1'))
-    place_piece(0, 2, Bishop.new('White', 'Player 1'))
-    place_piece(0, 3, Queen.new('White', 'Player 1'))
-    place_piece(0, 4, King.new('White', 'Player 1'))
-    place_piece(0, 5, Bishop.new('White', 'Player 1'))
-    place_piece(0, 6, Knight.new('White', 'Player 1'))
-    place_piece(0, 7, Rook.new('White', 'Player 1'))
+    place_piece(0, 0, Rook.new('White', player1))
+    place_piece(0, 1, Knight.new('White', player1))
+    place_piece(0, 2, Bishop.new('White', player1))
+    place_piece(0, 3, Queen.new('White', player1))
+    place_piece(0, 4, King.new('White', player1))
+    place_piece(0, 5, Bishop.new('White', player1))
+    place_piece(0, 6, Knight.new('White', player1))
+    place_piece(0, 7, Rook.new('White', player1))
     place_white_starting_pawns
   end
 
@@ -43,25 +45,25 @@ class GameBuilder
 
   def place_white_starting_pawns
     for i in 0..7
-      place_piece(1, i, Pawn.new('White', 'Player 1'))
+      place_piece(1, i, Pawn.new('White', player1))
     end
   end
 
   def place_black_starting_pieces
     place_black_starting_pawns
-    place_piece(7, 0, Rook.new('Black', 'Player 2'))
-    place_piece(7, 1, Knight.new('Black', 'Player 2'))
-    place_piece(7, 2, Bishop.new('Black', 'Player 2'))
-    place_piece(7, 3, Queen.new('Black', 'Player 2'))
-    place_piece(7, 4, King.new('Black', 'Player 2'))
-    place_piece(7, 5, Bishop.new('Black', 'Player 2'))
-    place_piece(7, 6, Knight.new('Black', 'Player 2'))
-    place_piece(7, 7, Rook.new('Black', 'Player 2'))
+    place_piece(7, 0, Rook.new('Black', player2))
+    place_piece(7, 1, Knight.new('Black', player2))
+    place_piece(7, 2, Bishop.new('Black', player2))
+    place_piece(7, 3, Queen.new('Black', player2))
+    place_piece(7, 4, King.new('Black', player2))
+    place_piece(7, 5, Bishop.new('Black', player2))
+    place_piece(7, 6, Knight.new('Black', player2))
+    place_piece(7, 7, Rook.new('Black', player2))
   end
 
   def place_black_starting_pawns
     for i in 0..7
-      place_piece(6, i, Pawn.new('Black', 'Player 2'))
+      place_piece(6, i, Pawn.new('Black', player2))
     end
   end
 end
